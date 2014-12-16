fce229b7d3e3e5315f2e190281ab16ebc2ef21d5
Jussi Malinen <jussi.ao.malinen@gmail.com>
2014-12-16 14:31:31 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from os.path import abspath, dirname, join
+import re
+
+NAME = 'robotbackgroundlogger'
+CLASSIFIERS = """
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Testing
+""".strip().splitlines()
+CURDIR = dirname(abspath(__file__))
+with open(join(CURDIR, NAME+'.py')) as f:
+    VERSION = re.search("\n__version__ = '(.*)'\n", f.read()).group(1)
+with open(join(CURDIR, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name             = NAME,
+    version          = VERSION,
+    author           = 'Robot Framework Developers',
+    author_email     = 'robotframework@gmail.com',
+    url              = 'https://github.com/robotframework/robotbackgroundlogger',
+    download_url     = 'https://pypi.python.org/pypi/robotbackgroundlogger',
+    license          = 'Apache License 2.0',
+    description      = 'A tool for logging to Robot Framework log '
+                       'from background threads.',
+    long_description = README,
+    keywords         = 'robotframework testing testautomation atdd',
+    platforms        = 'any',
+    classifiers      = CLASSIFIERS,
+    py_modules       = ['robotbackgroundlogger'],
+    install_requires = ['robotframework']
+)
