31ee9f98ea681024c80e15b8ac7533466428f604
Pekka Klärck <peke@iki.fi>
2017-01-09 02:48:25 +0200

    Explicitly make print function.
    PR #4 already changed usage to `print()`. Fixes #3.

--- robotbackgroundlogger.py
+++ robotbackgroundlogger.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from __future__ import with_statement
+from __future__ import print_function, with_statement
 
 try:
     from collections import OrderedDict
